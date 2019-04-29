9a7e074a308f4da6ec4c5d84fba545e7768ed878
Anthony Moss <asmoss93@gmail.com>
2019-04-29 09:32:26 -0400

    adds app as class and input component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Input from './Input';
+import Timer from './Timer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>React Timer</p>
-        <Input />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+      input: 0,
+      status: false,
+      done: false,
+      adder: []
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1>End time in seconds to use Timer!</h1>
+          <Timer time={this.state.time} />
+          {this.state.adder.map((one, i) => {
+            return (
+              <Timer time={this.state.time} key={i} />
+            )
+          })}
+          <br></br>
+          <button onClick={this._addTimer}>Add Timer</button>
+        </header>
+      </div>
+    );
+  }
+  _addTimer = () => {
+    this.addOne = 1
+    this.setState({
+      adder: this.state.adder.concat(this.addOne)
+    })
+  }
 }
+
 
 export default App;
--- src/Input.js
+++ src/Input.js
@@ -4,6 +4,7 @@ class Input extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            inputText: "",
             time: this.props.time || 0
         }
     }
@@ -13,15 +14,26 @@ class Input extends React.Component {
             <div>
                 <h1>
                     {(this.state.time)} seconds left
+                    {(this._updateInputText)}
                 </h1>
             </div>
         )
     }
+
+    _updateInputText = (e) => {
+
+        console.log('onChange event fired');
+        console.log(e.target.value);
+    
+        // This is a request for a change
+        // that will take place in the future.
+        this.setState({
+        inputText: e.target.value
+        }, () => {
+        console.log(`After setState, State is now ${this.state.inputText}`);
+        });
+    }
 }
-
-
-
-
 
 
 export default Input;
